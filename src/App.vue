336a70b41812050bbd318b1ff7252e396f45daf0
mg-kh <tragon18@gmail.com>
2021-06-28 17:46:11 +0630

    fb-share-card-fix2

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,11 @@
 
 <script>
 import TheNavbar from "@/components/TheNavbar";
-let rootPath = "https://clean-design.netlify.app/";
+// let rootPath = "https://clean-design.netlify.app/";
 export default {
   data() {
     return {
-      logo: rootPath + require("./assets/images/coffee_cup.png"),
+      // logo: rootPath + require("./assets/images/coffee_cup.png"),
     };
   },
   components: {
@@ -23,7 +23,11 @@ export default {
       { property: "og:title", content: "Vue static web design" },
       { property: "og:site_name", content: "Clean Design" },
       { property: "og:type", content: "website" },
-      { property: "og:image", content: this.logo },
+      {
+        property: "og:image",
+        content:
+          "https://clean-design.netlify.app//assets/images/coffee_cup.png",
+      },
       {
         property: "og:description",
         content: "Transform coffee into ideas!",
